d3e9f84a90f2df4999a37aefc35cd9a234db7e12
Vijith Assar <github@vijithassar.com>
2021-03-16 20:07:47 -0400

    chore(core): play audio

--- source/audio.js
+++ source/audio.js
@@ -2,10 +2,42 @@ import * as d3 from 'd3';
 import { lineData } from './data';
 import { noop } from './helpers';
 
+const context = new AudioContext();
+
+const root = 440;
+const tuning = 0;
+const duration = 0.25;
+
 const audioDispatcher = d3.dispatch('play');
 
+const note = (frequency, start) => {
+  const oscillator = context.createOscillator();
+  const gain = context.createGain();
+
+  oscillator.connect(gain);
+  gain.connect(context.destination);
+
+  oscillator.frequency.value = frequency;
+
+  oscillator.start(context.currentTime + start);
+  oscillator.stop(context.currentTime + start + duration * 0.8);
+};
+
+const notes = (values) => {
+  const scale = d3
+    .scaleLinear()
+    .domain(d3.extent(values, (d) => d.value))
+    .range([root + tuning, (root + tuning) * 2]);
+
+  const pitches = values.map(({ value }) => scale(value));
+
+  pitches.forEach((pitch, index) => {
+    note(pitch, index * duration);
+  });
+};
+
 audioDispatcher.on('play', (values) => {
-  return;
+  notes(values);
 });
 
 const audio = (specification) => {
