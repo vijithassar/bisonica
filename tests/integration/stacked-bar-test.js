8861ea070fe0a2652db6befc15af681b1cca8887
Vijith Assar <github@vijithassar.com>
2022-05-25 19:41:36 -0400

    chore(charts): test horizontal stacked bar chart

--- tests/integration/stacked-bar-test.js
+++ tests/integration/stacked-bar-test.js
@@ -5,7 +5,7 @@ import { specificationFixture } from '../test-helpers.js';
 const { module, test } = qunit;
 
 module('integration > stacked-bar', function () {
-  test('renders a stacked bar chart', async function (assert) {
+  test('renders a vertical stacked bar chart', async function (assert) {
     const spec = specificationFixture('stackedBar');
     const element = render(spec);
 
@@ -25,5 +25,31 @@ module('integration > stacked-bar', function () {
       'all mark rects have positive numbers as height attributes',
     );
     assert.ok(!nodesHaveZeroHeights, 'some mark rects have nonzero height attributes');
+  });
+
+  test('renders a horizontal stacked bar chart', async function (assert) {
+    const spec = specificationFixture('stackedBar');
+
+    const { x, y } = spec.encoding;
+
+    // invert the encodings
+    spec.encoding.x = y;
+    spec.encoding.y = x;
+
+    const element = render(spec);
+
+    const mark = testSelector('mark');
+
+    assert.ok(element.querySelector(mark));
+    assert.equal(element.querySelector(mark).tagName, 'rect');
+
+    const nodes = [...element.querySelectorAll(mark)];
+    const nodeHasPositiveWidth = (node) => Number(node.getAttribute('width')) >= 0;
+    const nodeHasZeroWidth = (node) => Number(node.getAttribute('width')) === 0;
+    const nodesHavePositiveWidths = nodes.every(nodeHasPositiveWidth);
+    const nodesHaveZeroWidths = nodes.every(nodeHasZeroWidth);
+
+    assert.ok(nodesHavePositiveWidths, 'all mark rects have positive numbers as width attributes');
+    assert.ok(!nodesHaveZeroWidths, 'some mark rects have nonzero width attributes');
   });
 });
