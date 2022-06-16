fec8606e6c29add264ec094dd4f05a12ebb3292e
Vijith Assar <github@vijithassar.com>
2022-06-16 15:27:12 -0400

    fix(tooling): custom lint rules

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+const config = {
+    extends: ['@commitlint/config-conventional'],
+    "rules": {
+        'body-case': [0],
+        'body-max-line-length': [0],
+        'footer-empty': [0],
+        'references-empty': [0],
+        'signed-off-by': [0],
+    },
+};
+
+module.exports = config;
