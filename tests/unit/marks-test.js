22b7c8a505c303d39097497032d2d2fb48560e9d
Vijith Assar <github@vijithassar.com>
2022-07-19 14:21:38 -0400

    chore(tests): clean up test module structure
    Organize the existing tests.

--- tests/unit/marks-test.js
+++ tests/unit/marks-test.js
@@ -8,7 +8,7 @@ const { module, test } = qunit;
 const set = () => null
 
 module('unit > marks', () => {
-  test('bar width', (assert) => {
+  module('bar width', () => {
 
     const stackedBarChartSpec = specificationFixture('stackedBar');
     const categoricalBarChartSpec = specificationFixture('categoricalBar');
@@ -19,46 +19,55 @@ module('unit > marks', () => {
 
     const categories = categoricalBarChartSpec.data.values.map((item) => item.label).length;
 
-    assert.equal(
-      typeof barWidth(stackedBarChartSpec, dimensions),
-      'number',
-      'bar width is a number',
-    );
+    test('return value', (assert) => {
+      assert.equal(
+        typeof barWidth(stackedBarChartSpec, dimensions),
+        'number',
+        'bar width is a number',
+      );
+    });
 
-    assert.ok(
-      barWidth(stackedBarChartSpec, dimensions) <= dimensions.x / dates,
-      'time series bar width sized according to number of timestamps',
-    );
+    test('temporal encoding', (assert) => {
+      assert.ok(
+        barWidth(stackedBarChartSpec, dimensions) <= dimensions.x / dates,
+        'time series bar width sized according to number of timestamps',
+      );
+    });
 
-    assert.ok(
-      barWidth(categoricalBarChartSpec, dimensions) <= dimensions.x / categories,
-      'time series bar width sized according to number of categories',
-    );
+    test('nominal encoding', (assert) => {
+      assert.ok(
+        barWidth(categoricalBarChartSpec, dimensions) <= dimensions.x / categories,
+        'time series bar width sized according to number of categories',
+      );
+    });
 
-    const twoBarsTimeSeries = [
-      { label: '2020-01-01', value: 10, group: 'a' },
-      { label: '2020-01-01', value: 20, group: 'b' },
-      { label: '2020-01-02', value: 30, group: 'a' },
-      { label: '2020-01-02', value: 40, group: 'b' },
-    ];
+    test('mutation', (assert) => {
+      const twoBarsTimeSeries = [
+        { label: '2020-01-01', value: 10, group: 'a' },
+        { label: '2020-01-01', value: 20, group: 'b' },
+        { label: '2020-01-02', value: 30, group: 'a' },
+        { label: '2020-01-02', value: 40, group: 'b' },
+      ];
 
-    const twoBarsCategorical = [
-      { group: 'a', value: 10 },
-      { group: 'b', value: 20 },
-    ];
+      const twoBarsCategorical = [
+        { group: 'a', value: 10 },
+        { group: 'b', value: 20 },
+      ];
 
-    set(stackedBarChartSpec.data, 'values', twoBarsTimeSeries);
+      set(stackedBarChartSpec.data, 'values', twoBarsTimeSeries);
 
-    set(categoricalBarChartSpec.data, 'values', twoBarsCategorical);
+      set(categoricalBarChartSpec.data, 'values', twoBarsCategorical);
 
-    assert.ok(
-      barWidth(stackedBarChartSpec, dimensions) <= dimensions.x / 3,
-      'gap left between two time series bars',
-    );
+      assert.ok(
+        barWidth(stackedBarChartSpec, dimensions) <= dimensions.x / 3,
+        'gap left between two time series bars',
+      );
 
-    assert.ok(
-      barWidth(categoricalBarChartSpec, dimensions) <= dimensions.x / 3,
-      'gap left between two categorical bars',
-    );
+      assert.ok(
+        barWidth(categoricalBarChartSpec, dimensions) <= dimensions.x / 3,
+        'gap left between two categorical bars',
+      );
+    });
+
   });
 });
