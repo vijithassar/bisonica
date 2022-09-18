9fc8e24b0a6965217573a355849186713dd6e2d2
Vijith Assar <github@vijithassar.com>
2022-09-18 18:52:08 -0400

    chore(core): remove error from extension lookup
    It's tempting to want to design this to be an extremely disciplined API which can emit a useful descriptive error. Unfortunately in practice this function is likely to be used as both a check and a retrieval - *if* the specification has extended usermeta, then return the extension and use it. This leaves us with a choice: either keep the error and add a separate function for hasExtension() or similar, or else remove the error so the main function can be used in conditional logic without throwing when the condition is true. The latter seems more elegant, at least for now.

--- source/extensions.js
+++ source/extensions.js
@@ -1,8 +1,6 @@
 const extension = (s, key) => {
     if (s.usermeta?.[key]) {
         return s.usermeta[key];
-    } else {
-        throw new Error(`${key} is missing from specification.usermeta`);
     }
 };
 
