b859f8c6d3b66b712bab9c7c8c19b8125946ed7b
Vijith Assar <github@vijithassar.com>
2022-11-30 03:11:54 -0500

    chore(core): delete alignment calculation
    This no longer works now that the node is detached from the live DOM, because that in turn means that getBoundingClientRect can't calculate real values. Even if it did work, it would be slow. Good riddance!

--- source/marks.js
+++ source/marks.js
@@ -651,9 +651,6 @@ const textMarks = (s, dimensions) => {
 		text.attr('text-anchor', s.mark.align || 'middle')
 		text.attr('alignment-baseline', s.mark.baseline || 'baseline')
 
-		const dy = text.node().getBoundingClientRect().height * 0.25
-
-		text.attr('transform', `translate(0,${dy})`)
 		const hasLink = !!(s.encoding && encodingValue(s, 'href'))
 		text.classed('link', hasLink)
 	}
