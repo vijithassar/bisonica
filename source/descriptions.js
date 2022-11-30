26a7fe815f08eae530972f6adb785bdd1ed5f7a2
Vijith Assar <github@vijithassar.com>
2022-11-29 22:46:53 -0500

    chore(tooling): add placeholder for array destructuring

--- source/descriptions.js
+++ source/descriptions.js
@@ -11,7 +11,7 @@ const delimiter = '; '
 
 const quantitativeChannels = (s) => {
 	const result = Object.entries(s.encoding)
-		.filter(([, definition]) => {
+		.filter(([_, definition]) => {
 			return definition.type === 'quantitative'
 		})
 		.map(([channel]) => channel)
