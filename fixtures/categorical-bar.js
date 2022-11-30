2e6305e38230471bdf917e38f3f5276554bd08c9
Vijith Assar <github@vijithassar.com>
2022-11-30 18:35:20 -0500

    chore(tooling): lint fixes for test fixtures

--- fixtures/categorical-bar.js
+++ fixtures/categorical-bar.js
@@ -1,21 +1,21 @@
 const categoricalBarChartSpec = {
-  $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-  title: { text: 'Categorical Bar Chart' },
-  data: {
-    values: [
-      { animal: 'rabbit', value: 31 },
-      { animal: 'cow', value: 25 },
-      { animal: 'snake', value: 25 },
-      { animal: 'elephant', value: 25 },
-      { animal: 'mouse', value: 24 },
-    ],
-  },
-  mark: { type: 'bar', tooltip: true },
-  encoding: {
-    x: { field: 'animal', type: 'nominal' },
-    y: { title: 'count', field: 'value', type: 'quantitative' },
-    color: { field: null, type: 'nominal' },
-  },
-};
+	$schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+	title: { text: 'Categorical Bar Chart' },
+	data: {
+		values: [
+			{ animal: 'rabbit', value: 31 },
+			{ animal: 'cow', value: 25 },
+			{ animal: 'snake', value: 25 },
+			{ animal: 'elephant', value: 25 },
+			{ animal: 'mouse', value: 24 }
+		]
+	},
+	mark: { type: 'bar', tooltip: true },
+	encoding: {
+		x: { field: 'animal', type: 'nominal' },
+		y: { title: 'count', field: 'value', type: 'quantitative' },
+		color: { field: null, type: 'nominal' }
+	}
+}
 
-export { categoricalBarChartSpec };
+export { categoricalBarChartSpec }
--- fixtures/rules.js
+++ fixtures/rules.js
@@ -1,21 +1,21 @@
 const rulesSpec = {
-  data: {
-    values: [
-      { group: 'a', value: 2 },
-      { group: 'b', value: 4 },
-      { group: 'c', value: 7 },
-    ],
-  },
-  title: { text: 'Rules' },
-  mark: 'rule',
-  encoding: {
-    y: {
-      field: 'value',
-      type: 'quantitative',
-    },
-    size: { value: 2 },
-    color: { field: 'group', type: 'nominal' },
-  },
-};
+	data: {
+		values: [
+			{ group: 'a', value: 2 },
+			{ group: 'b', value: 4 },
+			{ group: 'c', value: 7 }
+		]
+	},
+	title: { text: 'Rules' },
+	mark: 'rule',
+	encoding: {
+		y: {
+			field: 'value',
+			type: 'quantitative'
+		},
+		size: { value: 2 },
+		color: { field: 'group', type: 'nominal' }
+	}
+}
 
-export { rulesSpec };
+export { rulesSpec }
