30d4dea8a80d8fb656f98a3a9e9d93647907bdfc
Vijith Assar <github@vijithassar.com>
2023-03-28 16:43:13 -0400

    chore(core): separate functions for formatting types
    The formatting functions for numbers, time, and UTC time all need different defaults which will be used in cases where no format string is present in the input, so it's easier to split them into separate functions.

--- source/format.js
+++ source/format.js
@@ -3,6 +3,42 @@ import { identity } from './helpers.js'
 import * as d3 from 'd3'
 
 /**
+ * create a time formatting function
+ * @param {object} config encoding or axis definition object
+ * @returns {function(Date)} date formatting function
+ */
+const timeFormat = config => {
+	if (!config?.format) {
+		return date => date.toString()
+	}
+	return d3.timeFormat(config.format)
+}
+
+/**
+ * create a time formatting function in UTC
+ * @param {object} config encoding or axis definition object
+ * @returns {function(Date)} UTC date formatting function
+ */
+const utcFormat = config => {
+	if (!config?.format) {
+		return date => date.toUTCString()
+	}
+	return d3.utcFormat(config.format)
+}
+
+/**
+ * create a number formatting function
+ * @param {object} config encoding or axis definition object
+ * @returns {function(number)} number formatting function
+ */
+const numberFormat = config => {
+	if (!config?.format) {
+		return number => number.toString()
+	}
+	return d3.format(config.format)
+}
+
+/**
  * create a formatting function
  * @param {object} config encoding or axis definition object
  * @returns {function(string|number|Date)} formatting function
@@ -13,9 +49,14 @@ const _format = config => {
 	}
 	const time = config.type === 'temporal' || config.formatType === 'time' || config.timeUnit
 	if (time) {
-		return d3.timeFormat(config.format)
+		const utc = !!config.timeUnit?.startsWith('utc')
+		if (utc) {
+			return utcFormat(config)
+		} else {
+			return timeFormat(config)
+		}
 	} else {
-		return d3.format(config.format)
+		return numberFormat(config)
 	}
 }
 const format = memoize(_format)
