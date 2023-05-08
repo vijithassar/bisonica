9dc493b3cbe0299c21cb4757ece48348b10113eb
Vijith Assar <github@vijithassar.com>
2023-05-08 15:48:13 -0400

    feat(core): download data
    Add links to download data in CSV and JSON format. CSV format returns the output of the values() function and thus should always be equivalent to the content of the table and the chart, whereas JSON format returns the Vega Lite specification.

---
+++ source/download.js
@@ -0,0 +1,40 @@
+import { extension } from './extensions.js'
+import { noop } from './helpers.js'
+import { values } from './values.js'
+import { csvFormat } from 'd3'
+
+const formats = ['csv', 'json']
+
+/**
+ * render download links
+ * @param {object} s Vega Lite specification
+ * @returns {function(object)} download link renderer
+ */
+const download = s => {
+	if (extension(s, 'download') === null) {
+		return noop
+	}
+	const handler = event => {
+		const format = event.target.innerText
+		let file
+		if (format === 'csv') {
+			file = new Blob([csvFormat(values(s))], { type: 'text/csv' })
+		} else if (format === 'json') {
+			file = new Blob([JSON.stringify(s)], { type: 'text/json' })
+		}
+		window.open(URL.createObjectURL(file))
+	}
+	return selection => {
+		selection.append('h3').text('download')
+		formats.forEach(format => {
+			if (extension(s, 'download')?.[format] !== false) {
+				selection
+					.append('a')
+					.text(format)
+					.on('click', handler)
+			}
+		})
+	}
+}
+
+export { download }
