837060c49da1a4762c48ba9a162f52dc84741486
Vijith Assar <github@vijithassar.com>
2023-05-08 23:48:19 -0400

    chore(tests): keyboard navigation test helper
    Factor out a helper function for running keyboard navigation tests to reduce repetitive boilerplate when adding new sets of key presses to test.

--- tests/integration/keyboard-test.js
+++ tests/integration/keyboard-test.js
@@ -7,27 +7,33 @@ import qunit from 'qunit'
 
 const { module, test } = qunit
 
-module('integration > keyboard', function() {
-	const dispatchEvents = {
-		left: new KeyboardEvent('keyup', {
-			key: 'ArrowLeft',
-			bubbles: true
-		}),
-		right: new KeyboardEvent('keyup', {
-			key: 'ArrowRight',
-			bubbles: true
-		})
+const dispatchEvents = {
+	left: new KeyboardEvent('keyup', {
+		key: 'ArrowLeft',
+		bubbles: true
+	}),
+	right: new KeyboardEvent('keyup', {
+		key: 'ArrowRight',
+		bubbles: true
+	})
+}
+
+const testNavigation = (assert, s, steps) => {
+	const element = render(s)
+	const first = element.querySelector(testSelector('mark'))
+	let current
+	element.addEventListener('keyup', () => {
+		current = element.querySelector('[data-highlight]')
+	})
+	for (let { key, content } of steps) {
+		first.dispatchEvent(dispatchEvents[key])
+		assert.equal(current.getAttribute('aria-label'), content)
 	}
+}
+
+module('integration > keyboard', function() {
 	test('keyboard navigation works', function(assert) {
 	    const s = specificationFixture('circular')
-
-		const element = render(s)
-		const first = element.querySelector(testSelector('mark'))
-		let current
-		element.addEventListener('keyup', () => {
-			current = element.querySelector('[data-highlight]')
-		})
-
 		const steps = [
 			{ key: 'right', content: 'value: 8; group: A' },
 			{ key: 'right', content: 'value: 4; group: B' },
@@ -35,10 +41,6 @@ module('integration > keyboard', function() {
 			{ key: 'left', content: 'value: 4; group: B' },
 			{ key: 'right', content: 'value: 2; group: C' }
 		]
-
-		for (let { key, content } of steps) {
-			first.dispatchEvent(dispatchEvents[key])
-			assert.equal(current.getAttribute('aria-label'), content)
-		}
+		testNavigation(assert, s, steps)
 	})
 })
