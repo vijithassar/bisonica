7176ff4202f7eba60b5706d706f71f69af20d47e
Vijith Assar <github@vijithassar.com>
2023-05-11 13:42:04 -0400

    feat(core): support top level dimensions in specification
    If a dimensions object is not provided to the chart() function as an input argument, fall back to checking the specification for height and width properties and substitute those if they are found.

---
+++ tests/integration/chart-test.js
@@ -0,0 +1,28 @@
+import qunit from 'qunit'
+import { chart } from '../../source/chart.js'
+import { testSelector, specificationFixture } from '../test-helpers.js'
+import * as d3 from 'd3'
+
+const { module, test } = qunit
+
+module('unit > chart', () => {
+	const dimensions = { x: 500, y: 500 }
+	test('creates a chart function with separate dimensions object', assert => {
+		const s = specificationFixture('circular')
+		const selection = d3.create('svg:svg').append('svg:g')
+		const renderer = chart(s, dimensions)
+		selection.call(renderer)
+		const marks = selection.selectAll(testSelector('mark'))
+		assert.equal(marks.size(), s.data.values.length)
+	})
+	test('creates a chart function with dimensions in specification', assert => {
+		const s = specificationFixture('circular')
+		s.width = dimensions.x
+		s.height = dimensions.y
+		const selection = d3.create('svg:svg').append('svg:g')
+		const renderer = chart(s)
+		selection.call(renderer)
+		const marks = selection.selectAll(testSelector('mark'))
+		assert.equal(marks.size(), s.data.values.length)
+	})
+})
