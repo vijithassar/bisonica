382b0e0140c929bfdfaad0c9802cd19bcbfdf49b
Vijith Assar <github@vijithassar.com>
2023-05-14 16:22:54 -0400

    chore(tests): add module structure for expression tests

--- tests/unit/expression-test.js
+++ tests/unit/expression-test.js
@@ -4,28 +4,32 @@ import { expression } from '../../source/expression.js'
 const { module, test } = qunit
 
 module('expression', () => {
-	test('interpolates properties', assert => {
-		const exp = "'>-' + datum.a"
-		const datum = { a: '•' }
-		assert.equal(expression(exp)(datum), '>-•')
+	module('concatenation', () => {
+		test('interpolates properties', assert => {
+			const exp = "'>-' + datum.a"
+			const datum = { a: '•' }
+			assert.equal(expression(exp)(datum), '>-•')
+		})
+		test('interpolates multiple properties', assert => {
+			const exp = "'>-' + datum.a + '-' + datum.b"
+			const datum = { a: '•', b: '*' }
+			assert.equal(expression(exp)(datum), '>-•-*')
+		})
+		test('omits malformed string interpolations', assert => {
+			const exp = "'>' + '-' + datum.a + '-*"
+			const datum = { a: '•' }
+			assert.equal(expression(exp)(datum), '>-•')
+		})
 	})
-	test('interpolates multiple properties', assert => {
-		const exp = "'>-' + datum.a + '-' + datum.b"
-		const datum = { a: '•', b: '*' }
-		assert.equal(expression(exp)(datum), '>-•-*')
-	})
-	test('omits malformed string interpolations', assert => {
-		const exp = "'>' + '-' + datum.a + '-*"
-		const datum = { a: '•' }
-		assert.equal(expression(exp)(datum), '>-•')
-	})
-	test('random()', assert => {
-		const exp = 'random()'
-		assert.equal(typeof expression(exp)(), 'number')
-	})
-	test('now()', assert => {
-		const exp = 'now()'
-		assert.equal(typeof expression(exp)(), 'number')
-		assert.equal(expression(exp)(), Date.now())
+	module('functions', () => {
+		test('random()', assert => {
+			const exp = 'random()'
+			assert.equal(typeof expression(exp)(), 'number')
+		})
+		test('now()', assert => {
+			const exp = 'now()'
+			assert.equal(typeof expression(exp)(), 'number')
+			assert.equal(expression(exp)(), Date.now())
+		})
 	})
 })
