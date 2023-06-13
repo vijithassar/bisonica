7ced25f7d1a18848c55aed52788640cda2c8fccc
Vijith Assar <github@vijithassar.com>
2023-06-13 00:07:17 -0400

    chore(core): centralize menu
    Assemble menu content in a centralized location.

---
+++ source/menu.js
@@ -0,0 +1,39 @@
+import { feature } from './feature.js'
+import { extension } from './extensions.js'
+import { download } from './download.js'
+
+/**
+ * determine menu content
+ * @param {object} s Vega Lite specification
+ * @returns {string[]} menu items
+ */
+const items = s => {
+	const download = feature(s).hasDownload() && extension(s, 'download')
+	return [
+		download?.csv !== false ? 'csv' : null,
+		download?.json !== false ? 'json' : null
+	].filter(Boolean)
+}
+
+/**
+ * render menu
+ * @param {object} s Vega Lite specification
+ * @returns {function(object)} menu renderer
+ */
+const menu = s => {
+	return selection => {
+		selection
+			.select('.menu')
+			.append('ul')
+			.selectAll('li')
+			.data(items(s))
+			.enter()
+			.append('li')
+			.attr('data-menu', item => item)
+			.append('a')
+			.text(item => item)
+			.attr('href', item => download(s, item))
+	}
+}
+
+export { menu }
