97e52c3ab3b22bb8dd129bc736dbf1b0fcef4f4b
Vijith Assar <github@vijithassar.com>
2023-06-28 23:23:27 -0400

    chore(core): add margin type

--- source/types.d.js
+++ source/types.d.js
@@ -9,3 +9,13 @@
 * encoding channel in cartesian space
 * @typedef {'x'|'y'} cartesian
 */
+
+/**
+* margin convention object
+* @typedef margin
+* @property {number} top top margin
+* @property {number} right right margin
+* @property {number} bottom bottom margin
+* @property {number} left left margin
+* @see {@link https://observablehq.com/@d3/margin-convention|margin convention}
+*/
