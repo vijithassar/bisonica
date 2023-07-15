efeb1728fa1fb3ec2bfc81ff32835135507432ec
Vijith Assar <github@vijithassar.com>
2023-07-15 16:37:11 -0400

    feat(core): gradients

---
+++ source/gradient.js
@@ -0,0 +1,58 @@
+import { key, noop } from './helpers.js'
+
+/**
+ * assemble a string key for a gradient
+ * @param {object} s Vega Lite specification
+ * @param {number} [index] gradient index
+ * @return {string} gradient id
+ */
+const gradientKey = (s, index = 0) => {
+	const n = index + 1
+	return `${key(s.title.text)}-gradient-${n}`
+}
+
+/**
+ * create a gradient
+ * @param {object} s Vega Lite specification
+ * @return {function(object)} gradient definition rendering function
+ */
+const gradient = s => {
+	if (!s.mark?.color?.gradient) {
+		return noop
+	}
+	const renderer = selection => {
+		if (selection.node().tagName !== 'defs') {
+			throw new Error('gradients can only be rendered into a <defs> node')
+		}
+		const colors = [
+			s.mark.color,
+			s.layer?.map(item => item.mark.color)
+		]
+			.filter(Boolean)
+			.filter(item => item.gradient)
+		colors.forEach((color, index) => {
+			const type = `${color.gradient}Gradient`
+			const gradient = selection.append(type)
+			gradient
+				.attr('id', gradientKey(s, index))
+				.attr('x1', color.x1)
+				.attr('x2', color.x2)
+				.attr('y1', color.y1)
+				.attr('y2', color.y2)
+			if (type === 'radialGradient') {
+				gradient.attr('r1', color.r1)
+				gradient.attr('r2', color.r2)
+			}
+			gradient
+				.selectAll('stop')
+				.data(color.stops)
+				.enter()
+				.append('stop')
+				.attr('offset', d => d.offset)
+				.attr('stop-color', d => d.color)
+		})
+	}
+	return renderer
+}
+
+export { gradient, gradientKey }
