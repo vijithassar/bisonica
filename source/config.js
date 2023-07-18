b0e2bf2ad4f9d3354722eb477e0d462f974734f5
Vijith Assar <github@vijithassar.com>
2023-07-18 00:18:20 -0400

    chore(docs): document configuration variables

--- source/config.js
+++ source/config.js
@@ -3,10 +3,22 @@
  * @module config
  */
 
+/**
+ * class name for outer <g> node wrapper
+ * @type {string}
+ */
 const WRAPPER_CLASS = 'wrapper'
 
+/**
+ * increment for positions that are not derived from data
+ * @type {number}
+ */
 const GRID = 10
 
+/**
+ * minimum number of ticks for a continuous scale axis
+ * @type {number}
+ */
 const MINIMUM_TICK_COUNT = 3
 
 export {
