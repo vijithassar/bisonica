9ad9d85cdf416aed91a88c94c9f438f98246a9cd
Vijith Assar <github@vijithassar.com>
2023-07-23 14:31:24 -0400

    chore(core): test top level descriptions

--- tests/unit/description-test.js
+++ tests/unit/description-test.js
@@ -1,6 +1,6 @@
 import qunit from 'qunit'
 import { specificationFixture } from '../test-helpers.js'
-import { axisDescription } from '../../source/descriptions.js'
+import { axisDescription, chartDescription } from '../../source/descriptions.js'
 
 const { module, test } = qunit
 
@@ -18,6 +18,37 @@ module('unit > description', () => {
 					assert.ok(valid, `generates ${channel} axis description`)
 				})
 			})
+		})
+	})
+
+	module('chart', () => {
+		const specification = () => {
+			return {
+				mark: {
+					type: 'arc'
+				},
+				encoding: {
+					theta: {
+						field: 'size',
+						type: 'quantitative'
+					},
+					color: {
+						field: 'type',
+						type: 'nominal'
+					}
+				}
+			}
+		}
+		test('generates chart description based on encodings', assert => {
+			const s = specification()
+			const description = chartDescription(s)
+			assert.equal(description, 'pie chart of size split by type')
+		})
+		test('prepends custom chart description', assert => {
+			const s = specification()
+			s.description = 'this is a chart with a custom description! it is also a'
+			const description = chartDescription(s)
+			assert.equal(description, 'this is a chart with a custom description! it is also a pie chart of size split by type')
 		})
 	})
 })
